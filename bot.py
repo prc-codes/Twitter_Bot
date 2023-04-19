9506536e3bd66b6abe946d81000d297eafffd670
Prithvi Raj Chauhan <prithviraj91201@gmail.com>
2023-04-19 16:16:08 +0530

    second addition

--- bot.py
+++ bot.py
@@ -12,4 +12,6 @@ client = tweepy.Client(bearer_token, api_key, api_key_secret, access_token, acce
 auth = tweepy.OAuth1UserHandler(api_key, api_key_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
-client.create_tweet(text= "Hello, Twitter API")
+# client.create_tweet(text= "Hello, Twitter API")
+client.create_tweet(text = "yes")
+
