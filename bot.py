87475754ca6da534388dafb8e3f012b959518a13
Prithvi Raj Chauhan <prithviraj91201@gmail.com>
2023-04-20 12:37:40 +0530

    changes

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import tweepy
 
-api_key = "nI91vHpbiqw04221mLI1v6olv"
-api_key_secret = "F3PMOTaH717v03TEcu45plP0mTcpIxEQjRTetNQXAMzD2zW1PD"
-bearer_token = r"AAAAAAAAAAAAAAAAAAAAAMBQmwEAAAAATXEkOQ%2F6SgIRCquwRR0F3n9kkIE%3DI0KGDn8vvAQLczIyZFUGQz759CLF9TIqKZEKOlgZV19d596V7n"
-access_token = "957524978118770690-aKakkA0hrvNyUJQDKinPn2L3of33cTV"
-access_token_secret = "Kjy3Nqx2MZqOTJHdqE41XyPc3wnXq5bUH3BoN4NdOyoCk"
+api_key = ""
+api_key_secret = ""
+bearer_token = r""
+access_token = ""
+access_token_secret = ""
 
 
 # Establishing a connection with Twitter API
@@ -14,4 +14,3 @@ api = tweepy.API(auth)
 
 # client.create_tweet(text= "Hello, Twitter API")
 client.create_tweet(text = "yes")
-
